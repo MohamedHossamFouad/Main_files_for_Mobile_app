e2a10a4a6516359cb7adf65b1f5225ea0afb9411
Mohamed Hossam Fouad <71577798+MohamedHossamFouad@users.noreply.github.com>
2021-07-13 22:20:57 +0200

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,23 @@
+// import 'package:Dart_Mastering_Level_1/helpers.dart';
+// void main () {
+//   print('hello ')
+//   msg();
+
+// }
+
+void main(){
+  String name="mohamed";
+  print('Welcome from dart ${name}');
+}
+
+
+
+
+
+
+
+
+
+
+
+
